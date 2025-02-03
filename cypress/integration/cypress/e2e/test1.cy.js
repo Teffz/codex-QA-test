8da6d482eb00da73e4ec797089545d6626b59cb9
Sthefane <101375596+Teffz@users.noreply.github.com>
2025-02-03 00:51:43 -0300

    Update test1.cy.js

--- cypress/integration/cypress/e2e/test1.cy.js
+++ cypress/integration/cypress/e2e/test1.cy.js
@@ -9,14 +9,14 @@ describe('Formulário de Contato', () => {
     cy.get('#email').type('sthefane@gmail.com'); // E-mail
     cy.get('#mensagem').type('Gostaria de mais informações sobre os pacotes de viagem.'); // Mensagem
 
-    // Defina o comportamento esperado para o alert
-    cy.on('window:alert', (alertText) => {
-      expect(alertText).to.contains('Mensagem enviada com sucesso!');
-    });
 
     // Clica no botão de envio do formulário
     cy.contains('button', 'Enviar').click();  // Clica no botão com o texto 'Enviar'
+
+        // Defina o comportamento esperado para o alert
+        cy.on('window:alert', (alertText) => {
+          expect(alertText).to.contains('Mensagem enviada com sucesso!');
+        });
   });
 });
-
 
